19718c3a427efe9f8e546de5f9d4e83aeb6ca108
tennga <tenngawang6080@gmail.com>
2020-11-16 13:29:57 -0500

    testing tobe and toEqual

--- functions.js
+++ functions.js
@@ -5,6 +5,14 @@ const sortDown = (arr) => {
     return arr.sort((a,b) => b - a)
 }
 
+const reverseString = (str) => {
+    const splitStr = str.split('');
+    const reversedSplitStr = splitStr.reverse();
+    const joinReverse = reversedSplitStr.join('');
+    return joinReverse;
+}
+
 module.exports.sortDown = sortDown;
 
 module.exports.sortUp = sortUp;
+module.exports.reverseString = reverseString;
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,16 @@ const sort= require('./functions');
 test('Sort the array in Increasing Order:', () => {
     expect(sort.sortUp([2,5,1,23,3,0,12])).toEqual([0,1,2,3,5,12,23]);
 });
+
+test('Sort must return empty array when passed empty array:', () => {
+    expect(sort.sortUp([])).toEqual([]);
+});
+
+test('Sort the array in Decreasing Order: ', () => {
+    expect(sort.sortDown([1,2,3])).toEqual([3,2,1]);
+});
+
+test('Reverse String: ', () => {
+    expect(sort.reverseString("apple")).toBe("elppa");
+});
+
